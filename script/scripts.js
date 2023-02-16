dcbefa05929abe034934dfbf763ff3754d1715fe
gr402_hudal <dimahudobin05@gmail.com>
2023-02-16 13:48:28 +0700

    изменения в выводе массива

--- script/scripts.js
+++ script/scripts.js
@@ -4,15 +4,14 @@ let n = 0;
 let m = 0;
 let k = 0;
 
-alert (enter());
-alert (conclusion());
-alert (addition());
-alert (conclusion());
+alert(enter())
+alert(conclusion())
+
 
 
 //ввод массива объектов
 function enter(){
-    n = prompt("Введите количество учеников", 1)
+    n = prompt("Введите количество учеников", 2)
     for (let i = 0; i < n; i++){
         arr[i] = {
             surname: prompt('Введите фамилию ученика','Худобин'),
@@ -21,13 +20,13 @@ function enter(){
             sex: prompt('Введите пол ученика','Мужской'),
             nationality: prompt('Введите национальность ученика','Русский'),
             growth: prompt('Введите рост ученика в см','174'),
-            wheight: prompt('Введите вес ученика в кг','59'),
+            weight: prompt('Введите вес ученика в кг','59'),
             /*year: prompt('Введите год рождения ученика','2004'),
             mount: prompt('Введите месяц рождения ученика','март'),
             number: prompt('Введите число рождения ученика','1'),
             phone_number: prompt('Введите номер телефона ученика','89618900104'),
             postcode: prompt('Введите почтовый индекс ученика','716036'),
-            contry: prompt('Введите страну проживания ученика','Россия'),
+            country: prompt('Введите страну проживания ученика','Россия'),
             region: prompt('Введите область проживания ученика','Томская область'),
             district: prompt('Введите район проживания ученика','Томский район'),
             town: prompt('Введите город проживания ученика','Томск'),
@@ -37,19 +36,24 @@ function enter(){
             school: prompt('Введите номер школы','53'),
             class: prompt('Введите номер класса','9'),*/
         };
-    };
-    k += n;
-    return arr;
-    // return 'Заполнение завершено усепшно';
-};
+    }
+    return alert('Заполнение ученика прошло успешно');
+}
 
 //вывод массива объектов
 function conclusion(){
-    for (let i = 0; i < n; i++){
-        arr1[i] = (i+1+'. ') + Object.values(arr[i]) + '\n';
-    };
-    return arr1;
-};
+    for (let i in arr){
+        if (i === '0'){
+            arr1.push(`Ученик №${i}:\n`)
+        }else{arr1.push(`\nУченик №${i}:\n`)}
+        console.log(i)
+        for (let key in arr[i]){
+            arr1.push(key + ': ' + arr[i][key] + ', ')
+        }
+    }
+    console.log(arr1)
+    return arr1.join('');
+}
 
 //функция дополнения
 function addition(){
@@ -63,13 +67,13 @@ function addition(){
             sex: prompt('Введите пол ученика','Мужской'),
             nationality: prompt('Введите национальность ученика','Русский'),
             growth: prompt('Введите рост ученика в см','174'),
-            wheight: prompt('Введите вес ученика в кг','59'),
+            weight: prompt('Введите вес ученика в кг','59'),
             /*year: prompt('Введите год рождения ученика','2004'),
             mount: prompt('Введите месяц рождения ученика','март'),
             number: prompt('Введите число рождения ученика','1'),
             phone_number: prompt('Введите номер телефона ученика','89618900104'),
             postcode: prompt('Введите почтовый индекс ученика','716036'),
-            contry: prompt('Введите страну проживания ученика','Россия'),
+            country: prompt('Введите страну проживания ученика','Россия'),
             region: prompt('Введите область проживания ученика','Томская область'),
             district: prompt('Введите район проживания ученика','Томский район'),
             town: prompt('Введите город проживания ученика','Томск'),
@@ -80,11 +84,11 @@ function addition(){
             class: prompt('Введите номер класса','9'),*/
         };
         n++;
-    };
+    }
     arr.push.apply(arr, arr2);
     return arr;
     // return 'Данные успешно записаны';
-};
+}
 
 
 /*вывод массива объектов
